d49a434d1817bb9f8d21030e2ef176586124e842
Josh Sharrer <jossharr@uat.edu>
2018-11-20 13:56:02 -0800

    Initial Commit

---
+++ Header and Source Code/Header and Source Code/Functions.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>;
+
+using namespace std;
+
+string GetTextFromUser(string prompt) // Declare and initialize the string function GetTextFromUser
+{
+	string userTextString; // Declare and initialize the string userTextString
+	cout << prompt;        // Output the string prompt that has been passed to it 
+	cin >> userTextString; // Receive the user input and store it as a variable
+	return userTextString; // Return to the main function with the result of running this function
+}
+
+int GetNumberFromUser(string prompt)
+{
+	int userIntegerString;          // Declare and initialize the string userIntegerString
+	cout << prompt;                 // Output the string prompt that has been passed to it 
+	cin >> userIntegerString;       // Receive the user input and store it as a variable
+	return userIntegerString;       // Return to the main function with the result of running this function
+
+}
+
+string GetSpeciesFromUser(string prompt) // Declare and initialize the string function GetSpeciesFromUser
+{
+	string userFavSpecies; // Declare and initialize the string userTextString
+	cout << prompt;        // Output the string prompt that has been passed to it 
+	cin >> userFavSpecies; // Receive the user input and store it as a variable
+	return userFavSpecies; // Return to the main function with the result of running this function
+}
---
+++ Header and Source Code/Header and Source Code/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>;
+
+using namespace std;
+
+void GetTextFromUser(); // Declare the function GetTextFromUser
+
+void GetNumberFromUser(); // Declare the function GetNumberFromUser
+
+void GetSpeciesFromUser(); // Declare the function GetSpeciesFromUser
+
+int main()
+{
+	string response_1 = GetTextFromUser("What is your favorite color? \n"); // Save the string response_1 as the result of GetTextFromUser("What is your favorite color? ")
+	int response_2 = GetNumberFromUser("What is your favorite number? \n"); // Save the integer response_2 as the result of GetNumberFromUser("What is your favorite number? ")
+	string favorite_species = GetSpeciesFromUser("What is your favorite animal species? \n"); // Save the string favorite_species as the result of GetSpeciesFromUser("What is your favorite animal species? ");
+	cout << "\nYour favorite color is " << response_1 << ", and your favorite number is " << response_2 << "." << endl;
+	cout << "Your favorite color species of animals is also " << favorite_species << "." << endl;
+	return 0; // Return Statement
+}
